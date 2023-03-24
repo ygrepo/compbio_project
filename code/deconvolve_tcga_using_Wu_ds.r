ce1b23c7d923458691115d635d2c8c27c6c00e27
Yves Greatti <ygrepo@gmail.com>
2023-03-24 18:20:39 -0400

    Updated code

--- code/deconvolve_tcga_using_Wu_ds.r
+++ code/deconvolve_tcga_using_Wu_ds.r
@@ -1,46 +1,42 @@
 rm(list = ls())
 # load
-library(AnnotationDbi)
-library(EnsDb.Hsapiens.v79)
+# library(AnnotationDbi)
+# library(EnsDb.Hsapiens.v79)
 #library(MuSiC)
 library(SingleCellExperiment)
 library(dplyr)
-library(nnls)
+#library(nnls)
 
 setwd("~/github/compbio_project/code")
 
-source("music/utils.R")
-source("music/construct.R")
-source("music/analysis.R")
+# source("music/utils.R")
+# source("music/construct.R")
+# source("music/analysis.R")
 
 tcga_file = paste("../data/brca/tcga/processed/", 
-                  "convert_genes_unstranded.rds", sep="")
-tcga <-readRDS(tcga_file)
+                  "converted_genes_normal_tissue_unstranded_exp_data.rds", 
+                  sep="")
+exprs <-readRDS(tcga_file)
 
 
-exprs <- as.matrix(tcga[, -1])
-# take only two first samples
-exprs <- exprs[,1:2]
-rownames(exprs) <- rownames(tcga)
-
-
-sc_file = paste("../data/brca/Wu_etal_2021_BRCA_bulk_sc/processed/", 
-                 "small_wu_etal_sce.rds", sep="")
-sc_data <- readRDS(file=sc_file)
-ct <- unique(sc_data$cellType)
+sce_file = paste("../data/brca/Wu_etal_2021_BRCA_bulk_sc/processed/", 
+                 "sce_major_cell_type.rds", 
+                 sep="")
+sce <- readRDS(sce_file)
+ct <- unique(sce$cellType)
 
 
 
 #debug(music_prop)
 # Estimate cell type proportions
-# debug(music_prop)
-prop = music_prop(bulk.mtx = exprs, sc.sce = sc_data, 
+prop = music_prop(bulk.mtx = exprs, 
+                                sc.sce = sce, 
                                 clusters = 'cellType', 
                                 samples = 'Barcode',
                                 select.ct = ct,
                                 verbose = TRUE)
 prop_file = paste("../data/brca/tcga/processed/Wu/", 
-                        "03_24_small_tcga_small_sc_prop.csv", sep="")
-saveRDS(prop_cells, file=Est.prop.TCGA_prop)
+                        "prop_normal_tissue_unstranded_major_ct.rds", sep="")
+saveRDS(prop, file=prop_file)
 
 #undebug(music_prop)
